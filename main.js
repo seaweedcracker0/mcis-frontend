fca801b3b03bccafb34edd50d820d5e9230c2d9d
Sun <sun.wang@3enix.com>
2021-12-16 01:36:41 +0800

    1. update the code to post form data.

--- main.js
+++ main.js
@@ -1,253 +1,230 @@
 angular.module('MCIS', []).
-    directive("fileread", [function () {
-        return {
-            scope: {
-                fileread: "="
-            },
-            link: function (scope, element, attributes) {
-                element.bind("change", function (changeEvent) {
-                    var reader = new FileReader();
-                    reader.onload = function (loadEvent) {
-                        scope.$apply(function () {
-                            scope.fileread = loadEvent.target.result;
-                        });
-                    }
-                    reader.readAsDataURL(changeEvent.target.files[0]);
-                });
-            }
-        }
-    }]).
-    controller('formController', function($scope, $http) {
-    $scope.formParams = {};
-    $scope.stage = "";
-    $scope.section = "";
-    $scope.apiEndPoint = "";
-    $scope.stagePolicy = false;
-    var inputMin = 12;
-
-    $scope.init = function() {
-        $scope.initSignaturePad();
-    }
-
-    $scope.stagePolicyFn = function() {
-        $scope.stagePolicy = true;
-    }
-
-    $scope.nextSection = function(section) {
-        $scope.formValidation = true;
-
-        if ($scope.multiStepForm.$valid) {
-            $scope.direction = 1;
-            $scope.section = section;
-            $scope.formValidation = false;
+directive("fileread", [function () {
+    return {
+        scope: {
+            fileread: "="
+        },
+        link: function (scope, element, attributes) {
+            element.bind("change", function (changeEvent) {
+                var reader = new FileReader();
+                reader.onload = function (loadEvent) {
+                    scope.$apply(function () {
+                        scope.fileread = loadEvent.target.result;
+                    });
+                }
+                reader.readAsDataURL(changeEvent.target.files[0]);
+            });
         }
     }
+}]).
+controller('formController', function($scope, $http) {
+$scope.formParams = {};
+$scope.stage = "";
+$scope.section = "";
+$scope.apiEndPoint = "";
+$scope.stagePolicy = false;
+var inputMin = 12;
 
-    $scope.backSection = function(section) {
-        $scope.direction = 0;
-        $scope.section = section;
-    }
+$scope.init = function() {
+    $scope.initSignaturePad();
+}
 
-    // Navigation functions
-    $scope.nextStep = function (stage) {
-    //$scope.direction = 1;
-    //$scope.stage = stage;
+$scope.stagePolicyFn = function() {
+    $scope.stagePolicy = true;
+}
 
+$scope.nextSection = function(section) {
     $scope.formValidation = true;
 
-        if ($scope.multiStepForm.$valid) {
-            $scope.direction = 1;
-            $scope.stage = stage;
-            $scope.formValidation = false;
-        }
-    };
-
-    $scope.backStep = function (stage) {
-        $scope.direction = 0;
-        $scope.stage = stage;
-    };
-
-    $scope.submitForm = function () {
-        var wsUrl = "http://localhost:8080/greeting";
-
-        // Check form validity and submit data using $http
-        if ($scope.multiStepForm.$valid) {
+    if ($scope.multiStepForm.$valid) {
+        $scope.direction = 1;
+        $scope.section = section;
         $scope.formValidation = false;
+    }
+}
 
-        $http({
-            method: 'POST',
-            url: wsUrl,
-            data: JSON.stringify($scope.formParams)
-        }).then(function successCallback(response) {
-            if (response
-            && response.data
-            && response.data.status
-            && response.data.status === 'success') {
-            $scope.stage = "success";
-            } else {
-            if (response
-                && response.data
-                && response.data.status
-                && response.data.status === 'error') {
-                $scope.stage = "error";
-            }
-            }
-        }, function errorCallback(response) {
-            $scope.stage = "error";
-            console.log(response);
-        });
-        }
-    };
-      
-    $scope.signhere = function () {
-        var sign = document.getElementById("esign-modal");
-        sign.classList.add('show');
+$scope.backSection = function(section) {
+    $scope.direction = 0;
+    $scope.section = section;
+}
+
+// Navigation functions
+$scope.nextStep = function (stage) {
+//$scope.direction = 1;
+//$scope.stage = stage;
+
+$scope.formValidation = true;
+
+    if ($scope.multiStepForm.$valid) {
+        $scope.direction = 1;
+        $scope.stage = stage;
+        $scope.formValidation = false;
+    }
+};
+
+$scope.backStep = function (stage) {
+    $scope.direction = 0;
+    $scope.stage = stage;
+};
+
+$scope.submitForm = function (e) {
+    // var wsUrl = "http://localhost:8080/iweb-manager/user/ERecruit-Test-Page.html";
+
+    // Check form validity and submit data using $http
+    if (!$scope.multiStepForm.$valid) {
+        e.preventDefault()
+    }
+}
+  
+$scope.signhere = function () {
+    var sign = document.getElementById("esign-modal");
+    sign.classList.add('show');
+}
+
+$scope.signdone = function () {
+    var sign = document.getElementById("esign-modal");
+    sign.classList.remove('show');
+}
+
+$scope.reset = function() {
+// Clean up scope before destorying
+$scope.formParams = {};
+$scope.stage = "";
+}
+
+$scope.textChanged = function(nric, type) {
+    if (type == 'personal'){
+        if (nric.length >= inputMin) $scope.getICInfo(nric);
+    }else if (type == 'spouse'){
+        if (nric.length >= inputMin) $scope.getSpouseICInfo(nric);
+    }
+};
+
+$scope.getICInfo = function(nric) {
+    $scope.formParams.gender = Number(nric.substring(11, 12)) % 2 == 0 ? 'female' : 'male';
+    var dob = moment(nric.substring(0, 6), 'YYMMDD');
+    if (dob.isValid() && dob.isAfter(moment())) {
+        dob.subtract(100, 'years');
+    }
+    console.log(gender);
+
+    if (dob.isValid()){
+        console.log(dob);
+        console.log('YY:', dob.format('YYYY'));
+        console.log('DD:', dob.format('DD'));
+        console.log('MM:', dob.format('MM'));
+        $scope.formParams.personalDobDate = dob.format('DD');
+        $scope.formParams.personalDobMonth = dob.format('MM');
+        $scope.formParams.personalDobYear = dob.format('YYYY');            
+    }
+}
+
+$scope.getSpouseICInfo = function(nric) {
+    var dob = moment(nric.substring(0, 6), 'YYMMDD');
+    if (dob.isValid() && dob.isAfter(moment())) {
+        dob.subtract(100, 'years');
+    }
+    console.log(gender);
+
+    if (dob.isValid()){
+        console.log(dob);
+        console.log('YY:', dob.format('YYYY'));
+        console.log('DD:', dob.format('DD'));
+        console.log('MM:', dob.format('MM'));
+        $scope.formParams.spouseDobDate = dob.format('DD');
+        $scope.formParams.spouseDobMonth = dob.format('MM');
+        $scope.formParams.spouseDobYear = dob.format('YYYY');            
+    }
+}
+
+$scope.initSignaturePad = function() {
+    var wrapper = document.getElementById("signature-pad");
+    var wrapper2 = document.getElementById("esign-modal");
+    var clearButton = wrapper.querySelector("[data-action=clear]");
+    var confirmButton = wrapper2.querySelector("[data-action=confirm]");
+    var canvas = wrapper.querySelector("canvas");
+    var signaturePad = new SignaturePad(canvas, {
+        // It's Necessary to use an opaque color when saving image as JPEG;
+        // this option can be omitted if only saving as PNG or SVG
+        backgroundColor: 'rgb(220, 220, 220)',
+    });
+
+    // Adjust canvas coordinate space taking into account pixel ratio,
+    // to make it look crisp on mobile devices.
+    // This also causes canvas to be cleared.
+    function resizeCanvas() {
+        // When zoomed out to less than 100%, for some very strange reason,
+        // some browsers report devicePixelRatio as less than 1
+        // and only part of the canvas is cleared then.
+        var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+
+        // This part causes the canvas to be cleared
+        canvas.width = canvas.offsetWidth * ratio;
+        canvas.height = canvas.offsetHeight * ratio;
+        canvas.getContext("2d").scale(ratio, ratio);
+
+        // This library does not listen for canvas changes, so after the canvas is automatically
+        // cleared by the browser, SignaturePad#isEmpty might still return false, even though the
+        // canvas looks empty, because the internal data of this library wasn't cleared. To make sure
+        // that the state of this library is consistent with visual state of the canvas, you
+        // have to clear it manually.
+        signaturePad.clear();
     }
 
-    $scope.signdone = function () {
-        var sign = document.getElementById("esign-modal");
-        sign.classList.remove('show');
-    }
+    // On mobile devices it might make more sense to listen to orientation change,
+    // rather than window resize events.
+    window.onresize = resizeCanvas;
+    resizeCanvas();
 
-    $scope.reset = function() {
-    // Clean up scope before destorying
-    $scope.formParams = {};
-    $scope.stage = "";
-    }
-    
-    $scope.textChanged = function(nric, type) {
-        if (type == 'personal'){
-            if (nric.length >= inputMin) $scope.getICInfo(nric);
-        }else if (type == 'spouse'){
-            if (nric.length >= inputMin) $scope.getSpouseICInfo(nric);
-        }
-    };
+    function download(dataURL, filename) {
+        if (navigator.userAgent.indexOf("Safari") > -1 && navigator.userAgent.indexOf("Chrome") === -1) {
+            window.open(dataURL);
+        } else {
+            var blob = dataURLToBlob(dataURL);
+            var url = window.URL.createObjectURL(blob);
 
-    $scope.getICInfo = function(nric) {
-        $scope.formParams.gender = Number(nric.substring(11, 12)) % 2 == 0 ? 'female' : 'male';
-        var dob = moment(nric.substring(0, 6), 'YYMMDD');
-        if (dob.isValid() && dob.isAfter(moment())) {
-            dob.subtract(100, 'years');
-        }
-        console.log(gender);
+            var a = document.createElement("a");
+            a.style = "display: none";
+            a.href = url;
+            a.download = filename;
 
-        if (dob.isValid()){
-            console.log(dob);
-            console.log('YY:', dob.format('YYYY'));
-            console.log('DD:', dob.format('DD'));
-            console.log('MM:', dob.format('MM'));
-            $scope.formParams.personalDobDate = dob.format('DD');
-            $scope.formParams.personalDobMonth = dob.format('MM');
-            $scope.formParams.personalDobYear = dob.format('YYYY');            
+            document.body.appendChild(a);
+            a.click();
+
+            window.URL.revokeObjectURL(url);
         }
     }
 
-    $scope.getSpouseICInfo = function(nric) {
-        var dob = moment(nric.substring(0, 6), 'YYMMDD');
-        if (dob.isValid() && dob.isAfter(moment())) {
-            dob.subtract(100, 'years');
-        }
-        console.log(gender);
+    // One could simply use Canvas#toBlob method instead, but it's just to show
+    // that it can be done using result of SignaturePad#toDataURL.
+    function dataURLToBlob(dataURL) {
+    // Code taken from https://github.com/ebidel/filer.js
+    var parts = dataURL.split(';base64,');
+    var contentType = parts[0].split(":")[1];
+    var raw = window.atob(parts[1]);
+    var rawLength = raw.length;
+    var uInt8Array = new Uint8Array(rawLength);
 
-        if (dob.isValid()){
-            console.log(dob);
-            console.log('YY:', dob.format('YYYY'));
-            console.log('DD:', dob.format('DD'));
-            console.log('MM:', dob.format('MM'));
-            $scope.formParams.spouseDobDate = dob.format('DD');
-            $scope.formParams.spouseDobMonth = dob.format('MM');
-            $scope.formParams.spouseDobYear = dob.format('YYYY');            
-        }
+    for (var i = 0; i < rawLength; ++i) {
+        uInt8Array[i] = raw.charCodeAt(i);
     }
 
-    $scope.initSignaturePad = function() {
-        var wrapper = document.getElementById("signature-pad");
-        var wrapper2 = document.getElementById("esign-modal");
-        var clearButton = wrapper.querySelector("[data-action=clear]");
-        var confirmButton = wrapper2.querySelector("[data-action=confirm]");
-        var canvas = wrapper.querySelector("canvas");
-        var signaturePad = new SignaturePad(canvas, {
-            // It's Necessary to use an opaque color when saving image as JPEG;
-            // this option can be omitted if only saving as PNG or SVG
-            backgroundColor: 'rgb(220, 220, 220)',
-        });
-
-        // Adjust canvas coordinate space taking into account pixel ratio,
-        // to make it look crisp on mobile devices.
-        // This also causes canvas to be cleared.
-        function resizeCanvas() {
-            // When zoomed out to less than 100%, for some very strange reason,
-            // some browsers report devicePixelRatio as less than 1
-            // and only part of the canvas is cleared then.
-            var ratio =  Math.max(window.devicePixelRatio || 1, 1);
-
-            // This part causes the canvas to be cleared
-            canvas.width = canvas.offsetWidth * ratio;
-            canvas.height = canvas.offsetHeight * ratio;
-            canvas.getContext("2d").scale(ratio, ratio);
-
-            // This library does not listen for canvas changes, so after the canvas is automatically
-            // cleared by the browser, SignaturePad#isEmpty might still return false, even though the
-            // canvas looks empty, because the internal data of this library wasn't cleared. To make sure
-            // that the state of this library is consistent with visual state of the canvas, you
-            // have to clear it manually.
-            signaturePad.clear();
-        }
-
-        // On mobile devices it might make more sense to listen to orientation change,
-        // rather than window resize events.
-        window.onresize = resizeCanvas;
-        resizeCanvas();
-
-        function download(dataURL, filename) {
-            if (navigator.userAgent.indexOf("Safari") > -1 && navigator.userAgent.indexOf("Chrome") === -1) {
-                window.open(dataURL);
-            } else {
-                var blob = dataURLToBlob(dataURL);
-                var url = window.URL.createObjectURL(blob);
-
-                var a = document.createElement("a");
-                a.style = "display: none";
-                a.href = url;
-                a.download = filename;
-
-                document.body.appendChild(a);
-                a.click();
-
-                window.URL.revokeObjectURL(url);
-            }
-        }
-
-        // One could simply use Canvas#toBlob method instead, but it's just to show
-        // that it can be done using result of SignaturePad#toDataURL.
-        function dataURLToBlob(dataURL) {
-        // Code taken from https://github.com/ebidel/filer.js
-        var parts = dataURL.split(';base64,');
-        var contentType = parts[0].split(":")[1];
-        var raw = window.atob(parts[1]);
-        var rawLength = raw.length;
-        var uInt8Array = new Uint8Array(rawLength);
-
-        for (var i = 0; i < rawLength; ++i) {
-            uInt8Array[i] = raw.charCodeAt(i);
-        }
-
-        return new Blob([uInt8Array], { type: contentType });
-        }
-
-        clearButton.addEventListener("click", function (event) {
-            signaturePad.clear();
-        });
-
-        confirmButton.addEventListener("click", function (event) {
-            if (signaturePad.isEmpty()) {
-                alert("Please provide a signature first.");
-              } else {
-                var dataURL = signaturePad.toDataURL();
-                $scope.formParams.esigndraw = dataURL;
-                console.log('eSign Base64: ', $scope.formParams.esigndraw);
-                $scope.signdone();
-              }
-        })
+    return new Blob([uInt8Array], { type: contentType });
     }
+
+    clearButton.addEventListener("click", function (event) {
+        signaturePad.clear();
+    });
+
+    confirmButton.addEventListener("click", function (event) {
+        if (signaturePad.isEmpty()) {
+            alert("Please provide a signature first.");
+          } else {
+            var dataURL = signaturePad.toDataURL();
+            $scope.formParams.esigndraw = dataURL;
+            console.log('eSign Base64: ', $scope.formParams.esigndraw);
+            $scope.signdone();
+          }
+    })
+}
 });
